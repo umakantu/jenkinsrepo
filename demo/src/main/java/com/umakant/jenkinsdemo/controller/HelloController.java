172d4b7f66cb59ffacef9da1fb7f756dac176012
Umakant <Umakant@DESKTOP-O49BCT7>
2021-11-16 12:13:54 +0530

    16112021

---
+++ demo/src/main/java/com/umakant/jenkinsdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.umakant.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+@GetMapping("/hello")	
+public String helloDis()
+{
+	return "Welcome Uma";
+}
+}
